e2deceb4a37feb00a1b913a81c24f89774c7baac
Artūras Krištaponis <arturas@Arturass-MBP.lan>
2017-11-07 21:00:04 +0200

    Update

--- src/components/Play.js
+++ src/components/Play.js
@@ -24,6 +24,11 @@ export default class Play extends Component {
 			})
 	}
 
+	handleKeyPress(e) {
+		const press = this.handleSearch
+		e.key === "Enter" ? press() : null
+	}
+
 	render() {
 		return (
 			<div>
@@ -31,7 +36,7 @@ export default class Play extends Component {
 				<section className="play">
 					<div className="play-block1">
 						<div className="forma">
-							<input id="input" className="form-input" type="text" placeholder="search" />
+							<input id="input" className="form-input" type="text" onKeyPress={this.handleKeyPress.bind(this)} placeholder="search" />
 							<button onClick={this.handleSearch}>Show Joke</button>
 						</div>
 						<p>or</p>
